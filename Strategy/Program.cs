03208ed4e4f5a5cf865d2760d7cec67781fb62be
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-06-28 13:55:32 +0300

    Strategy Pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new After2010CreditCalculator();
+            customerManager.Save();
+         
+        }
+        abstract class CreditCalculatorBase
+        {
+            public abstract void Calculate();
+        }
+        class Before2010CreditCalculator : CreditCalculatorBase
+        {
+            public override void Calculate()
+            {
+                Console.WriteLine(" Credit calculated using before 2010 ");
+            }
+        }
+        class After2010CreditCalculator : CreditCalculatorBase
+        {
+            public override void Calculate()
+            {
+                Console.WriteLine("Credit calculated using before 2010");
+            }
+        }
+
+        class CustomerManager
+        {
+            public CreditCalculatorBase CreditCalculatorBase { get; set; }
+            public void Save()
+            {
+                CreditCalculatorBase.Calculate();
+                Console.WriteLine("Saved");
+            }
+        }
+
+    }
+}
