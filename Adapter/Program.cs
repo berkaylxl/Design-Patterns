2c7a6e40a94e8894726da221f30da92e62415a95
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-06-28 08:56:10 +0300

    Adapter Pattern

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -6,13 +6,21 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
-          
+            ProductManager productManager =new ProductManager(new BLoggerAdapter());
+            productManager.Add();
         }
         class ProductManager
         {
-
+            private ILogger _logger;
+            public ProductManager(ILogger logger)
+            {
+                _logger=logger;
+            }
+            public void Add()
+            {
+                _logger.Log("Loglama Tamamlandı");
+            }
         }
-
         interface ILogger
         {
             void Log(string message);
@@ -22,7 +30,22 @@ namespace Adapter
         {
             public void Log(string message)
             {
-                Console.WriteLine("A -> Log İşlemi Tamamlandı");
+                Console.WriteLine("A -> Log Completed");
+            }
+        }
+        class BLogger //Bir tool olarak gelebilir.Dokunamadığımız bir class olarak düşünmeliyiz
+        {
+            //işlemler
+            public void LogB(){
+                Console.WriteLine("B -> Log Completed");
+            }
+        }
+        class BLoggerAdapter : ILogger
+        {
+            public void Log(string message)
+            {
+                BLogger blogger = new BLogger();
+                blogger.LogB();
             }
         }
     }
