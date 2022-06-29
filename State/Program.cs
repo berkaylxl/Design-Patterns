4a173310905c4f80bbece28058a8891d7d0b1513
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-06-29 09:50:45 +0300

    State Pattern

---
+++ State/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Context context =new Context();
+          ModifiedState modifiedstate = new ModifiedState();
+          modifiedstate.DoAction(context);
+          Console.WriteLine(context.GetState());
+        }
+        interface IState
+        {
+            void DoAction(Context context);
+        }
+
+        class Context
+        {
+            private IState _state;
+            
+            public void SetState(IState state)
+            {
+                _state = state;
+            }
+            public IState GetState()
+            {
+                return _state;
+            }
+        }
+        class ModifiedState : IState
+        {
+            public void DoAction(Context context)
+            {
+                context.SetState(this);
+                Console.WriteLine("State : Modified");
+            }
+        }
+        class DeletedState : IState
+        {
+            public void DoAction(Context context)
+            {
+                context.SetState(this);
+                Console.WriteLine("State : Deleted");
+            }
+        }
+    }
+}
