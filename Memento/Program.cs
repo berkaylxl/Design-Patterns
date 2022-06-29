c22acb0383ba5c6bf714a0dec6ff69e0bd32e16a
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-06-29 09:21:11 +0300

    Template Method Pattern

---
+++ Memento/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Memento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Template Method/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Template_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoringAlgorithm algorithm;
+            algorithm = new MenScoringAlgorithm();
+            Console.WriteLine("ManScore: "+algorithm.GenerateScore(8,new TimeSpan(0,2,34)));
+
+            algorithm = new WomenScoringAlgorithm();
+            Console.WriteLine("WomenScore: " + algorithm.GenerateScore(8, new TimeSpan(0, 2, 55)));
+
+        }
+
+        abstract class ScoringAlgorithm
+        {
+            public int GenerateScore(int hits,TimeSpan time)
+            {
+                int score = CalculateBaseScore(hits);
+                int reduction = CalculateReducition(time);
+                return CalculateOverallScore(score, reduction);
+            }
+            public abstract int CalculateOverallScore(int score, int reduction);
+            public abstract int CalculateBaseScore(int hits);
+            public abstract int CalculateReducition(TimeSpan time);
+        }
+        class MenScoringAlgorithm : ScoringAlgorithm
+        {
+            public override int CalculateBaseScore(int hits)
+            {
+                return hits * 100;
+            }
+            public override int CalculateOverallScore(int score, int reduction)
+            {
+                return score - reduction;
+            }
+            public override int CalculateReducition(TimeSpan time)
+            {
+                return (int)time.TotalSeconds / 5;
+            }
+        }
+        class WomenScoringAlgorithm : ScoringAlgorithm
+        {
+            public override int CalculateBaseScore(int hits)
+            {
+                return hits * 100;
+            }
+            public override int CalculateOverallScore(int score, int reduction)
+            {
+                return score - reduction;
+            }
+            public override int CalculateReducition(TimeSpan time)
+            {
+                return (int)time.TotalSeconds / 3;
+            }
+        }
+    }
+}
